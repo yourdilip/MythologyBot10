68715bc1c82364905c7b820ba5d47f45eaaaacd0
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2023-11-04 16:18:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -61,7 +61,6 @@ async def start(client, message):
         )
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
-    if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
         except ChatAdminRequired:
